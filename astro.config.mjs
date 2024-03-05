2ff0281486c76bcf3184f55ef3f46c13234d4c20
--global <mattno5ss@gmail.com>
2024-03-05 14:58:54 -0600

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://m5ss-astro-tutorial.netlify.app/',
   integrations: [preact(), sitemap()]
 });
