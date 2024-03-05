58531269919953739ff49d6c1ece0a2256a15c87
--global <mattno5ss@gmail.com>
2024-03-05 14:28:06 -0600

    added an astro island

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
+import preact from "@astrojs/preact";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [preact(), sitemap()]
+});
